2bdbe83258e35f38318eab831fb5c1b385b7afb4
juancho <jm.ticona.p@gmail.com>
2022-06-28 23:24:24 -0500

    Modificación de Brenda

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom'
 import './App.css';
+import Actividad from './components/Actividad';
+import ActivityList from './components/ActivityList';
 
 function App() {
+
+  /**
+   * Creando lista en json de las entidades receptoras para el lado de los datos
+   * {
+   *  'title':'titulo de actividad',
+   *  'content':'Contenido de la actividad.'
+   * }
+   */
+  const makeJson = () => {
+    var objJson = [];
+    var titulos = document.getElementsByName("actividad_title");
+    var contenidos = document.getElementsByName("actividad_content");
+
+    for (let i = 0; i < titulos.length; i++) {
+      objJson.push(
+        {
+          'title': titulos[i].value,
+          'content': contenidos[i].value
+        }
+      );
+    }
+
+    return objJson;
+  }
+
+  var activityList = React.createElement(ActivityList,{},'');
+
+  const addActividad = () => {
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <button onClick={makeJson}>Mostrar JSON</button>
+      <button onClick={addActividad}>Agregar Actividad</button>
+      {activityList}
+    </>
+  )
+
+  // (
+  //   <>
+  //     <button onClick={makeJson}>Mostrar JSON</button>
+  //     <button onClick={addActividad}>Agregar Actividad</button>
+  //     <div name="actividad_list">
+  //       <Actividad></Actividad>
+  //       <Actividad></Actividad>
+  //     </div>
+  //   </>
+  // );
 }
 
 export default App;
