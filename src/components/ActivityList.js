a20214e9c5e18456af01fc130053a90badd66b24
juancho <jm.ticona.p@gmail.com>
2022-06-30 18:11:11 -0500

    Cambio de prueba en rama dev

--- src/components/ActivityList.js
+++ src/components/ActivityList.js
@@ -5,15 +5,6 @@ import Actividad from './Actividad'
 
 function ActivityList(props) {
 
-    //var arr_actividades = [];
-    //var conter=1;
-    /*
-    function addActividad(){
-      //var act = React.createElement(Actividad,{}, '');
-      //arr_actividades.push( act );
-      const element = document.querySelector("div[name='actividad_list']")
-      ReactDOM.createPortal(<Actividad />, element);
-    }*/
     const [actividades, setActividades] = useState([])
     const [ actualizacion, setActualizacion ] = useState(false)
     let arreglo = [...actividades]
